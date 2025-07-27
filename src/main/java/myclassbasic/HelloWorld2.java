69608586f6176f5a0c7c50a4e1aa6c1596b950b6
MohanCG <mohancg28@gmail.com>
2025-07-27 10:55:43 +0530

    New Code changes

--- src/main/java/myclassbasic/HelloWorld2.java
+++ src/main/java/myclassbasic/HelloWorld2.java
@@ -4,11 +4,11 @@ public class HelloWorld2 {
 	
 	public static void main(String[] args)
 	{
-		System.out.println("HelloWorld !!!");
+		System.out.println("Welcome to the World of DevOps!!!");
 	}
 
 	// code changes form b1 branch
 	public void m1() {
-		System.out.println("from method-1");
+		System.out.println("New Method-1");
 	}
 }
